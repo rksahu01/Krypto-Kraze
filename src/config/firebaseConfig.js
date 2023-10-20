8901ecfd1990718181f893770e7c0afae588d59c
Roshan <rksahuaaaa111@gmail.com.com>
2023-10-20 11:47:53 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAPHZIeYlC6ULuY_tXTshHflmD0ZRtYmTs",
+  authDomain: "krypto-kaze.firebaseapp.com",
+  projectId: "krypto-kaze",
+  storageBucket: "krypto-kaze.appspot.com",
+  messagingSenderId: "280989858467",
+  appId: "1:280989858467:web:e6f24d54a1ec7154d61d16"
+};
+
+export default firebaseConfig;
+
+
